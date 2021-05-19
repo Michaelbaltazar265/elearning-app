77e777f9b1236e4a2b94059d40dcb4d206d4a808
Michael <michaelbaltazar265@gmail.com>
2021-05-19 02:55:19 +0000

    Test Result

--- elearning-app/src/components/MyClasses/Test/JsTest.jsx
+++ elearning-app/src/components/MyClasses/Test/JsTest.jsx
@@ -2,6 +2,7 @@ import React, {useState} from "react";
 import imageCongrats from "./../../../images/congrats.jpeg";
 import "./test.css";
 import info from "./../../SignUp/signInInfo";
+import testResult from './testResult'; 
 
 const JsTest = (props) => { 
 console.log(props.type)
@@ -9,37 +10,37 @@ const questions = [
     {
         questionText: 'What must a variable begin with?',
         answerOptions: [
-            { answerText: 'HTML tag', isCorrect: false },
-            { answerText: 'CSS tag', isCorrect: false },
-            { answerText: 'A letter, $ or _', isCorrect: true },
-            { answerText: 'Nothing', isCorrect: false },
+            { answerText: 'HTML tag', isCorrect: false ,answerNum: 1 },
+            { answerText: 'CSS tag', isCorrect: false,answerNum: 1 },
+            { answerText: 'A letter, $ or _', isCorrect: true ,answerNum: 1},
+            { answerText: 'Nothing', isCorrect: false,answerNum: 1 },
         ],
     },
     {
         questionText: 'What is a variable?',
         answerOptions: [
-            { answerText: 'A loop', isCorrect: false },
-            { answerText: 'Container for a piece of data', isCorrect: true },
-            { answerText: 'Nothing', isCorrect: false },
-            { answerText: 'A file', isCorrect: false },
+            { answerText: 'A loop', isCorrect: false , answerNum: 2 },
+            { answerText: 'Container for a piece of data', isCorrect: true  , answerNum: 2},
+            { answerText: 'Nothing', isCorrect: false , answerNum: 2 },
+            { answerText: 'A file', isCorrect: false , answerNum: 2 },
         ],
     },
     {
         questionText: 'How do you declare variables?',
         answerOptions: [
-            { answerText: 'var, let and const keywords', isCorrect: true },
-            { answerText: 'variable', isCorrect: false },
-            { answerText: 'A = sign', isCorrect: false },
-            { answerText: 'A keyword', isCorrect: false },
+            { answerText: 'var, let and const keywords', isCorrect: true , answerNum: 3  },
+            { answerText: 'variable', isCorrect: false , answerNum: 3  },
+            { answerText: 'A = sign', isCorrect: false , answerNum: 3  },
+            { answerText: 'A keyword', isCorrect: false , answerNum: 3  },
         ],
     },
     {
         questionText: 'DOM stands for?',
         answerOptions: [
-            { answerText: 'Document Out Matter', isCorrect: false },
-            { answerText: 'Document Outter Mammals', isCorrect: false },
-            { answerText: 'Document Other Meaning', isCorrect: false },
-            { answerText: 'Document Object Model', isCorrect: true },
+            { answerText: 'Document Out Matter', isCorrect: false , answerNum: 4  },
+            { answerText: 'Document Outter Mammals', isCorrect: false , answerNum: 4 },
+            { answerText: 'Document Other Meaning', isCorrect: false , answerNum: 4  },
+            { answerText: 'Document Object Model', isCorrect: true , answerNum: 4  },
         ],
     },
 ];
@@ -47,6 +48,7 @@ const questions = [
 const [currentQuestion, setCurrentQuestion] = useState(0);
   const [showScore, setShowScore] = useState(false);
   const [score, setScore] = useState(0);
+  const [view, setView] = useState("");
   
   const handleAnswerOptionClick = (isCorrect) => {  
     if (isCorrect) {
@@ -68,7 +70,12 @@ const [currentQuestion, setCurrentQuestion] = useState(0);
       setCurrentQuestion(goBack);
     }
   };
+  const handleResult = () => { 
+    setView("result")
+  }
 
+  const percentage = (score/questions.length) * 100;
+  
   return (
     <div className="container">
       <div className="row">
@@ -80,25 +87,49 @@ const [currentQuestion, setCurrentQuestion] = useState(0);
             {showScore ? (
               <>
                 <div className="score-section">
-                  You scored {score} out of {questions.length}
+                  
+                  <button onClick={() => handleResult() } className="btn btn-primary">View Result</button>
                 </div>
-
                 { 
-                
-                    score >= questions.length -1 ?( 
-                        <div className="image-container-test">
-                        <img src={imageCongrats} alt="congrats" />
-                        <p className="image-text">{`${info.firstName} ${info.lastName}`}</p>
-                        <p className='image-course'>JavaScript</p>
-                      </div>
-                    ) : ( 
+                view === "result" ? ( 
+                  <>
+                  <div className='score-section'>
+                    You scored {score} out of {questions.length}
+                  </div>
+                  <div className='col-12'>
+                    {
+                      
+                      testResult.map((result) => ( 
+                       
+                   <p className={result.isCorrect.toString()}> {`${result.answerNum}. ${result.answerText}`} </p>
+                  
 
-                        <div className='row'>
-                            <h4>Please try again later and remember to study</h4>
-                        </div>
-                    )
+                        
+                      
+                      ))
+                    }
+                  </div>
+                  <br/>
+                  <p>{ percentage + "%" }</p>
+                  <p className='false'>Anything in Red is wrong</p>
+
+                  {percentage >= 65 ?( 
+        <div className="image-container-test">
+        <img src={imageCongrats} alt="congrats" />
+        <p className="image-text">{`${info.firstName} ${info.lastName}`}</p>
+        <p className='image-course'>JavaScript</p>
+      </div>
+    ) : ( 
+         
+        <div className='row'>
+            <h4>Please try again later and remember to study</h4>
+        </div>
+    ) 
+}
+
+                  </>
+                ): false
                 }
-               
               </>
             ) : (
               <>
@@ -118,14 +149,18 @@ const [currentQuestion, setCurrentQuestion] = useState(0);
                         key={questions.id}
                         value={answerOption.isCorrect}
                         onClick={(currentanswer) => {
+                          testResult.push(answerOption);
                           handleAnswerOptionClick(answerOption.isCorrect, answerOption);
                         }}
                       >
                         {answerOption.answerText}
                       </button>
-                    )
+
+                    ) 
                   )}
-                </div>
+                </div> 
+
+
 
                 {currentQuestion > 0 ? (
                   <div className="return">
@@ -137,7 +172,7 @@ const [currentQuestion, setCurrentQuestion] = useState(0);
                   false
                 )}
               </>
-            )}
+            )} 
           </div>
         </div>
       </div>
@@ -145,4 +180,4 @@ const [currentQuestion, setCurrentQuestion] = useState(0);
   );
 };
 
-export default JsTest;
+export default JsTest; 
---
+++ elearning-app/src/components/MyClasses/Test/testResult.js
@@ -0,0 +1,4 @@
+
+
+const testResult = []; 
+export default testResult;
